8b539402c623d36ee32fb7218c29c15f1d0f896e
Pasha.azd <ppasha.azadeh@gmail.com>
2022-12-14 13:43:08 +0330

    INSTALL rest framework

--- Booking/settings.py
+++ Booking/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
+
     'residence.apps.ResidenceConfig',
     'transportation.apps.TransportationConfig',
 ]
