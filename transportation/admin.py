3a5b35a144fa8046bd47f9c4cfadd3b4caa04755
Pasha.azd <ppasha.azadeh@gmail.com>
2022-12-20 18:06:23 +0330

    SETUP transportation admin panel (some details need to be fixed later)

--- transportation/admin.py
+++ transportation/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from transportation.models import *
+
+
+# Inlines ...
+class CompanyInline(admin.TabularInline):
+    model = Companies
+    extra = 2
+
+
+class FlightAdmin(admin.ModelAdmin):
+    search_fields = ('origin', 'destination', 'date', 'flight')
+    list_display = ('flight', 'origin', 'destination', 'company')
+
+    inlines = (CompanyInline,)
+
+
+admin.site.register(Flight)
