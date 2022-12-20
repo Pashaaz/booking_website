b618296b5eeaf1ff9cc3c5f88c003f6a743b05e4
Pasha.azd <ppasha.azadeh@gmail.com>
2022-12-20 13:03:53 +0330

    INSTALL users app -- SET AUTH_USER_MODEL

--- Booking/settings.py
+++ Booking/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     'residence.apps.ResidenceConfig',
     'transportation.apps.TransportationConfig',
     'book.apps.BookConfig',
+    'users.apps.UsersConfig',
+
 ]
 
 MIDDLEWARE = [
@@ -133,3 +135,5 @@ MEDIA_URL = 'media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.CustomUser'
