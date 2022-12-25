adc5a99182fbac0c6a82c99bfbb22677fdd6313e
Pasha.azd <ppasha.azadeh@gmail.com>
2022-12-25 20:54:22 +0330

    BugFix Booking.urls

--- Booking/urls.py
+++ Booking/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('residence', include('residence.urls'))
+    path('residence/', include('residence.urls'))
 ]
