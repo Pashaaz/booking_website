b654ffe2924e57460a504ad0ad2819a215bc33d9
Pasha.azd <ppasha.azadeh@gmail.com>
2023-01-01 20:20:36 +0330

    Add LoginFirst, LoginSecond serializers to users.api.serializers

--- users/api/serializers.py
+++ users/api/serializers.py
@@ -1,3 +1,6 @@
+from abc import ABC
+
+from django.core.validators import RegexValidator
 from rest_framework import serializers
 from users.models import (
     UserProfile,
@@ -22,3 +25,16 @@ class ProfileSerializer(serializers.ModelSerializer):
             'national_number', 'emergency_phone', 'date_of_birth', 'user',
             'card_number', 'bank_account_number', 'bank_sheba_number',
         )
+
+
+class LoginFirstSerializer(serializers.Serializer):
+    phone = serializers.IntegerField(required=True,
+                                     validators=[RegexValidator(r'^989[0-3,9]\d{8}$', 'Enter a valid phone number.',
+                                                                'invalid')])
+
+
+class LoginSecondSerializer(serializers.Serializer):
+    phone = serializers.IntegerField(required=True,
+                                     validators=[RegexValidator(r'^989[0-3,9]\d{8}$', 'Enter a valid phone number.',
+                                                                'invalid')])
+    code = serializers.CharField(max_length=6)
