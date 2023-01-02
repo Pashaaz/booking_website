e2e98c8654888b454b3b526cedbfb10bfbf0657e
Pasha.azd <ppasha.azadeh@gmail.com>
2023-01-02 17:22:49 +0330

    Add is_available, available_number methods --> HotelRoomBooking, FlightBooking models

--- book/models.py
+++ book/models.py
@@ -38,10 +38,31 @@ class HotelRoomBooking(AbstractBooking):
     def get_total_price(self):
         pass
 
+    def is_available(self, start_date, end_date, hotel_room_id):
+        queryset = HotelRoomBooking.objects.all().filter(hotel_room_id=hotel_room_id)
+        is_available = True
+
+        for item in queryset:
+            if start_date <= item.start_date <= end_date:
+                is_available = False
+
+            elif start_date <= item.end_date <= end_date:
+                is_available = False
+
+        return is_available
+
 
 class FlightBooking(AbstractBooking):
     user = models.ForeignKey(CustomUser, on_delete=models.DO_NOTHING, related_name='user_flights')
     flight = models.ForeignKey(Flight, on_delete=models.CASCADE, related_name='flight_bookings')
     is_valid = models.BooleanField(default=True)
+
+    def available_number(self, flight_id):
+        flight_capacity = Flight.objects.get(flight_id=flight_id).capacity
+        booked_number = FlightBooking.objects.all().filter(flight_id=flight_id, is_valid=True).count()
+
+        available_number = flight_capacity - booked_number
+
+        return available_number
 
 # End of main models ---------------------------------------------------------------------------------------------------
