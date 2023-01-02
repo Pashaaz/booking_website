a278cc7b22634cf73ddda0c0710167ee7696ba94
Pasha.azd <ppasha.azadeh@gmail.com>
2023-01-02 15:23:41 +0330

    Modify book.api.serializers

--- book/api/serializers.py
+++ book/api/serializers.py
@@ -1,15 +1,24 @@
 from rest_framework import serializers
 
 from book.models import HotelRoomBooking, FlightBooking
+from users.api.serializers import CustomUserSerializer
+from residence.api.serializers import HotelRoomSerializer
+from transportation.api.serializers import FlightSerializer
 
 
 class HotelRoomBookingSerializer(serializers.ModelSerializer):
+    user = CustomUserSerializer()
+    hotel_room = HotelRoomSerializer()
+
     class Meta:
         model = HotelRoomBooking
         fields = ('__all__',)
 
 
 class FlightBookingSerializer(serializers.ModelSerializer):
+    user = CustomUserSerializer()
+    flight = FlightSerializer()
+
     class Meta:
         model = FlightBooking
         fields = ('__all__',)
