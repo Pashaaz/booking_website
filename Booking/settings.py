80b7a8959521fc9aee0c8cdcddfc87bdcc003523
Pasha.azd <ppasha.azadeh@gmail.com>
2023-01-03 16:36:40 +0330

    Install django-filter package --> Add it to installed apps, Update requirments.txt

--- Booking/settings.py
+++ Booking/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'rest_framework_simplejwt',
+    'django_filters',
 
     'residence.apps.ResidenceConfig',
     'transportation.apps.TransportationConfig',
