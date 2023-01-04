945e90662fc20730436e36329aaa4720b670f726
Pasha.azd <ppasha.azadeh@gmail.com>
2023-01-04 12:53:16 +0330

    Check and Config urls

--- transportation/api/urls.py
+++ transportation/api/urls.py
@@ -1,6 +1,10 @@
 from rest_framework.routers import DefaultRouter
 
+from transportation.api.views import FlightViewSet
+
 router = DefaultRouter()
+
+router.register(r'flight', FlightViewSet, basename='flight')
 
 urlpatterns = [
 
--- users/api/urls.py
+++ users/api/urls.py
@@ -4,6 +4,7 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r'user', UserViewSet, basename='user')
+router.register(r'user/profile', UserProfileViewSet, basename='profile')
 
 
 urlpatterns = [
