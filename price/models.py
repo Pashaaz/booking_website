a8182cc8f3b6197b97fda72f38d56332823cc07e
Pasha.azd <ppasha.azadeh@gmail.com>
2023-01-04 18:49:28 +0330

    Add (AbstractPrice, HotelRoomPrice, FlightPrice) models to price.models

--- price/models.py
+++ price/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+from residence.models import HotelRoom
+from transportation.models import Flight
+
+
+class AbstractPrice(models.Model):
+    price = models.IntegerField()
+
+    class Meta:
+        abstract = True
+
+
+class HotelRoomPrice(AbstractPrice):
+    hotel_room = models.ForeignKey(HotelRoom, on_delete=models.CASCADE)
+
+
+class FlightPrice(AbstractPrice):
+    TYPE_CHOICES = (
+        (1, 'Economy'),
+        (2, 'Business'),
+        (3, 'First_Class'),
+    )
+
+    flight_type = models.SmallIntegerField(choices=TYPE_CHOICES)
+    flight = models.ForeignKey(Flight, on_delete=models.CASCADE)
