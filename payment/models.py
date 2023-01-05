31f727b4bc69df2368e29f4cfd0a22e2ae1b9f41
Pasha.azd <ppasha.azadeh@gmail.com>
2023-01-05 12:52:08 +0330

    Add payment log models

--- payment/models.py
+++ payment/models.py
@@ -1,11 +1,22 @@
 from django.db import models
 
+from book.models import HotelRoomBooking, FlightBooking
 from residence.models import HotelRoom
 from transportation.models import Flight
+from users.models import CustomUser
 
 
 class AbstractPrice(models.Model):
     price = models.IntegerField()
+
+    class Meta:
+        abstract = True
+
+
+class AbstractPaymentLog(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.DO_NOTHING)
+    date = models.DateTimeField(auto_now_add=True)
+    status = models.BooleanField(default=True)
 
     class Meta:
         abstract = True
@@ -24,3 +35,11 @@ class FlightPrice(AbstractPrice):
 
     flight_type = models.SmallIntegerField(choices=TYPE_CHOICES)
     flight = models.ForeignKey(Flight, on_delete=models.CASCADE)
+
+
+class HotelRoomPaymentLog(AbstractPaymentLog):
+    booking_obj = models.ForeignKey(HotelRoomBooking, on_delete=models.DO_NOTHING)
+
+
+class FlightPaymentLog(AbstractPaymentLog):
+    booking_obj = models.ForeignKey(FlightBooking, on_delete=models.DO_NOTHING)
