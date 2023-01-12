9b9a737a791982260499cca7a9235a27016dff7c
Pasha.azd <ppasha.azadeh@gmail.com>
2023-01-12 13:37:24 +0330

    Create viewpoints app and create comment models

---
+++ viewpoints/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ViewpointsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'viewpoints'
---
+++ viewpoints/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from django.conf import settings
+from residence.models import Hotels
+from transportation.models import Companies
+
+
+class AbstractComment(models.Model):
+    STATUS_CHOICES = (
+        (1, 'Created'),
+        (2, 'Approved'),
+        (3, 'Rejected'),
+        (4, 'Deleted')
+    )
+
+    parent = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+                             related_name='%(class)ss')
+    comment_body = models.TextField()
+
+    status = models.PositiveSmallIntegerField(choices=STATUS_CHOICES, default=1)
+    validated_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True,
+                                     related_name='validated_%(class)ss')
+
+    created_time = models.DateTimeField(auto_now_add=True)
+    modified_time = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class HotelComment(AbstractComment):
+    hotel = models.ForeignKey(Hotels, on_delete=models.CASCADE, related_name='hotel_comments')
+
+
+class CompanyComment(AbstractComment):
+    company = models.ForeignKey(Companies, on_delete=models.CASCADE, related_name='company_comments')
