24a1263e70d9e0a664db580f89dba6b73748a765
Pasha.azd <ppasha.azadeh@gmail.com>
2023-01-13 15:27:03 +0330

    Add price serializers --> payment.api.serializers

--- payment/api/serializers.py
+++ payment/api/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
 
-from payment.models import FlightPaymentLog, HotelRoomPaymentLog
+from payment.models import (
+    FlightPaymentLog,
+    HotelRoomPaymentLog,
+    HotelRoomPrice,
+    FlightPrice
+)
 
 
 class FlightPaymentLogSerializer(serializers.ModelSerializer):
@@ -13,3 +18,19 @@ class HotelRoomPaymentLogSerializer(serializers.ModelSerializer):
     class Meta:
         model = HotelRoomPaymentLog
         fields = ('id', 'user', 'date', 'status', 'booking_obj')
+
+
+class HotelRoomPriceSerializer(serializers.ModelSerializer):
+    iri_to = serializers.CharField(write_only=True, required=False)
+
+    class Meta:
+        model = HotelRoomPrice
+        fields = ('id', 'price', 'hotel_room', 'iri_to')
+
+
+class FlightPriceSerializer(serializers.ModelSerializer):
+    iri_to = serializers.CharField(write_only=True, required=False)
+
+    class Meta:
+        model = FlightPrice
+        fields = ('id', 'price', 'flight', 'iri_to')
