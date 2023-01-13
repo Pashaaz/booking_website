792cc7a584986c5f896d791c44b8d55a27c8e907
Pasha.azd <ppasha.azadeh@gmail.com>
2023-01-13 12:53:31 +0330

    Register viewpoitns.api.views's urls in viewpoints.api.urls

--- viewpoints/api/urls.py
+++ viewpoints/api/urls.py
@@ -1,3 +1,22 @@
+from rest_framework.routers import DefaultRouter
+
+from viewpoints.api.views import (
+    HotelCommentViewSet,
+    CompanyCommentViewSet,
+    HotelRatingViewSet,
+    CompanyRatingViewSet,
+)
+
+router = DefaultRouter()
+
+router.register(r'hotel/comment', HotelCommentViewSet, basename='hotel/comment')
+router.register(r'hotel/rating', HotelRatingViewSet, basename='hotel/rating')
+router.register(r'company/comment', CompanyCommentViewSet, basename='company/comment')
+router.register(r'company/rating', CompanyRatingViewSet, basename='company/rating')
+
+
 urlpatterns = [
 
 ]
+
+urlpatterns += router.urls
