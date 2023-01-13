7094e05e4121fe36744c4f2e1d45afc66a1165b6
Pasha.azd <ppasha.azadeh@gmail.com>
2023-01-13 10:57:29 +0330

    Config basic urls for viewpoints

--- Booking/urls.py
+++ Booking/urls.py
@@ -33,7 +33,8 @@ urlpatterns = [
     path('transport/', include('transportation.urls')),
     path('users/', include('users.urls')),
     path('book/', include('book.urls')),
-    path('payment/', include('payment.urls'))
+    path('payment/', include('payment.urls')),
+    path('viewpoint/', include('viewpoints.urls')),
     # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
