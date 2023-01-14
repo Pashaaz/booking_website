9d1c60cd87d80ea533e634fa852ba4196fdfe359
Pasha.azd <ppasha.azadeh@gmail.com>
2023-01-14 08:09:37 +0330

    Complete viewpoints.api.views

--- viewpoints/api/views.py
+++ viewpoints/api/views.py
@@ -17,54 +17,59 @@ from viewpoints.models import (
 
 
 class HotelCommentViewSet(mixins.CreateModelMixin,
-                          # mixins.ListModelMixin,
+                          mixins.ListModelMixin,
                           viewsets.GenericViewSet):
-    queryset = HotelComment.objects.all()
+
     authentication_classes = [JWTAuthentication]
     serializer_class = HotelCommentSerializer
     permission_classes = [IsAuthenticated]
 
-    # def get_queryset(self):
-    #     if self.action == 'list':
-    #         queryset = HotelComment.objects.all().filter(hotel_id=request.data.get())
+    def get_queryset(self):
+        queryset = HotelComment.objects.all()
+
+        if self.action == 'list':
+            queryset = HotelComment.objects.all().filter(hotel_id=self.request.data.get('hotel'))
 
 
 class CompanyCommentViewSet(mixins.CreateModelMixin,
-                            # mixins.ListModelMixin,
+                            mixins.ListModelMixin,
                             viewsets.GenericViewSet):
-    queryset = CompanyComment.objects.all()
     authentication_classes = [JWTAuthentication]
-    serializer_class = HotelCommentSerializer
+    serializer_class = CompanyCommentSerializer
     permission_classes = [IsAuthenticated]
 
-    # def get_queryset(self):
-    #     if self.action == 'list':
-    #         queryset = HotelComment.objects.all().filter(hotel_id=request.data.get())
+    def get_queryset(self):
+        queryset = CompanyComment.objects.all()
+
+        if self.action == 'list':
+            queryset = CompanyComment.objects.all().filter(hotel_id=self.request.data.get('hotel'))
 
 
 class HotelRatingViewSet(mixins.CreateModelMixin,
                          mixins.UpdateModelMixin,
-                         # mixins.ListModelMixin,
+                         mixins.ListModelMixin,
                          viewsets.GenericViewSet):
-    queryset = HotelRating.objects.all()
     authentication_classes = [JWTAuthentication]
-    serializer_class = HotelCommentSerializer
+    serializer_class = HotelRatingSerializer
     permission_classes = [IsAuthenticated]
 
-    # def get_queryset(self):
-    #     if self.action == 'list':
-    #         queryset = HotelComment.objects.all().filter(hotel_id=request.data.get())
+    def get_queryset(self):
+        queryset = HotelRating.objects.all()
+
+        if self.action == 'list':
+            queryset = HotelRating.objects.all().filter(hotel_id=self.request.data.get('hotel'))
 
 
 class CompanyRatingViewSet(mixins.CreateModelMixin,
                            mixins.UpdateModelMixin,
-                           # mixins.ListModelMixin,
+                           mixins.ListModelMixin,
                            viewsets.GenericViewSet):
-    queryset = CompanyRating.objects.all()
     authentication_classes = [JWTAuthentication]
-    serializer_class = HotelCommentSerializer
+    serializer_class = CompanyRatingSerializer
     permission_classes = [IsAuthenticated]
 
-    # def get_queryset(self):
-    #     if self.action == 'list':
-    #         queryset = HotelComment.objects.all().filter(hotel_id=request.data.get())
+    def get_queryset(self):
+        queryset = CompanyRating.objects.all()
+
+        if self.action == 'list':
+            queryset = CompanyRating.objects.all().filter(hotel_id=self.request.data.get('hotel'))
