0ada64449fd0a8bf8ceede955d52df902931e595
Pasha.azd <ppasha.azadeh@gmail.com>
2023-01-18 23:34:54 +0330

    Modify payment.api.serializers

--- payment/api/serializers.py
+++ payment/api/serializers.py
@@ -4,7 +4,8 @@ from payment.models import (
     FlightPaymentLog,
     HotelRoomPaymentLog,
     HotelRoomPrice,
-    FlightPrice
+    FlightPrice,
+    Currency,
 )
 
 
@@ -21,16 +22,46 @@ class HotelRoomPaymentLogSerializer(serializers.ModelSerializer):
 
 
 class HotelRoomPriceSerializer(serializers.ModelSerializer):
-    iri_to = serializers.CharField(write_only=True, required=False)
+    class Meta:
+        model = HotelRoomPrice
+        fields = ('id', 'price', 'hotel_room')
+
+
+class FlightPriceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FlightPrice
+        fields = ('id', 'price', 'flight')
+
+
+class HotelRoomCurrencySerializer(serializers.ModelSerializer):
+    iri_to = serializers.CharField(max_length=3, required=False)
 
     class Meta:
         model = HotelRoomPrice
         fields = ('id', 'price', 'hotel_room', 'iri_to')
 
+    def to_representation(self, instance):
+        if self['iri_to']:
+            currency = Currency.objects.get(iri_to=self['iri_to'])
+            price = int(self['price']) * currency.ratio
 
-class FlightPriceSerializer(serializers.ModelSerializer):
-    iri_to = serializers.CharField(write_only=True, required=False)
+            self['price'] = price
+
+        super(HotelRoomCurrencySerializer, self).to_representation(self)
+
+
+class FlightCurrencySerializer(serializers.ModelSerializer):
+    iri_to = serializers.CharField(max_length=3, required=False)
 
     class Meta:
         model = FlightPrice
         fields = ('id', 'price', 'flight', 'iri_to')
+
+    def to_representation(self, instance):
+        if self['iri_to']:
+            currency = Currency.objects.get(iri_to=self['iri_to'])
+            price = int(self['price']) * currency.ratio
+
+            self['price'] = price
+
+        super(FlightCurrencySerializer, self).to_representation(self)
